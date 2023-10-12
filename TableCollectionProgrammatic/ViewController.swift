6f66e853f0a3863a1dbfbb17a95fd05b7f775c87
Atil Samancioglu <samancioglu.atil@gmail.com>
2023-10-12 13:01:35 +0300

    Initial Commit

---
+++ TableCollectionProgrammatic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableCollectionProgrammatic
+//
+//  Created by Atil Samancioglu on 12.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
